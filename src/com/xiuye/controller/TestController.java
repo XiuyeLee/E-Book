549645c3b328a48f65f06974d5a71ff0d17c8429
xiuye <xiuye_engineer@outlook.com>
2016-05-28 23:08:15 +0800

    start using spring MVC

---
+++ src/com/xiuye/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.xiuye.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+
+public class TestController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("userinfo.do test");
+		return new ModelAndView("userinfo");
+		
+		
+	}
+
+}
---
+++ src/com/xiuye/logger/Logger.java
@@ -0,0 +1,48 @@
+package com.xiuye.logger;
+
+import java.util.Date;
+
+public class Logger {
+
+	private Class clazz;
+	
+	public Class getClazz() {
+		return clazz;
+	}
+
+
+	public void setClazz(Class clazz) {
+		this.clazz = clazz;
+	}
+
+
+	private Logger(){
+	
+		
+		
+		
+	}
+	
+	
+	public static Logger getLogger(Class clazz){
+		
+		Logger log = getDefault();
+		log.setClazz(clazz);
+		
+		return log;
+		
+		
+	}
+	
+	private static Logger getDefault(){
+		return LoggerHoler.instance;
+	}
+	private static class LoggerHoler{
+		private static final Logger instance = new Logger();
+	}
+	public synchronized void info(String message){
+		
+		System.out.println("INFO: 时间:"+new Date(System.currentTimeMillis())+" 类对象:"+clazz+" 内容:"+message);
+		
+	}
+}
