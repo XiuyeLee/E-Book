23b51c560453e1eef961d39807a44268bc552c07
xiuye <xiuye_engineer@outlook.com>
2016-06-07 16:25:03 +0800

    new changes

--- src/com/xiuye/views/UserinfoView.java
+++ src/com/xiuye/views/UserinfoView.java
@@ -1,12 +1,8 @@
 package com.xiuye.views;
 
-import java.io.IOException;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletResponse;
 
 import com.xiuye.logger.Logger;
 
